2e35ee8feeb9ce05498730b7d20e97aa2102e8b9
Monchusay <armydolgov@mail.ru>
2022-04-22 10:32:34 +0300

    0.0.2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import './App.css';
+import style from "./App.module.css"
 import Header from "./Header/Header";
+import NavBar from "./NavBar/NavBar";
+import { Routes, BrowserRouter, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-    </div>
+    <BrowserRouter>
+      <div className={style.App}>
+        <Header />
+        <NavBar />
+          <div className={style.MailObj}>
+          </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/NavBar/NavBar.js
@@ -0,0 +1,47 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import style from "./NavBar.module.css";
+import { AiOutlineShoppingCart } from "react-icons/ai";
+import { BsShopWindow } from "react-icons/bs";
+import { BiCar } from "react-icons/bi";
+import { AiOutlineQuestionCircle } from "react-icons/ai";
+import { AiOutlineFlag } from "react-icons/ai";
+
+const NavBar = () => {
+  return (
+    <nav className={style.NavBar}>
+      <div className={style.NavBarItem}>
+        <NavLink to={"/MainPage"}>
+          {" "}
+          <AiOutlineFlag className={style.NavBarIcon} /> Main Page{" "}
+        </NavLink>
+      </div>
+      <div className={style.NavBarItem}>
+        <NavLink to={"/ProductList"}>
+          {" "}
+          <BsShopWindow className={style.NavBarIcon} /> Product List{" "}
+        </NavLink>
+      </div>
+      <div className={style.NavBarItem}>
+        <NavLink to={"/Delivery"}>
+          {" "}
+          <BiCar className={style.NavBarIcon} /> Delivery{" "}
+        </NavLink>
+      </div>
+      <div className={style.NavBarItem}>
+        <NavLink to={"/AboutUs"}>
+          {" "}
+          <AiOutlineQuestionCircle className={style.NavBarIcon} /> About Us{" "}
+        </NavLink>
+      </div>
+      <div className={style.NavBarItem}>
+        <NavLink to={"/Cart"}>
+          {" "}
+          <AiOutlineShoppingCart className={style.NavBarIcon} /> Cart{" "}
+        </NavLink>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
